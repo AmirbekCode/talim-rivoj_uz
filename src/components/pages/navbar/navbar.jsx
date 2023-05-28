6cddbf51805aad4ed889959eea94562394282349
AmirbekCode <106994015+AmirbekCode@users.noreply.github.com>
2023-05-28 20:00:03 +0300

    upgrade erorrs style and code clear

--- src/components/pages/navbar/navbar.jsx
+++ src/components/pages/navbar/navbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import '../../css/navbar_css/Navbar.css';
-
+import Logo from '../../../assets/Logo.png'
 
 const Navbar = () => {
   
   return (
-    <nav>
+    <nav className='Navbar'>
+      <div className="logo-section">
+        <img src={Logo} className="Logo"/>
+      </div>
       <ul className="navbar">
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Team</a></li>
-        <li><a href="#">Eleltron platform</a></li>
-        <li><a href="#">Offerta</a></li>
+        <li>Courses</li>
+        <li>Team</li>
+        <li>Eleltron platform</li>
+        <li>Offerta</li>
       </ul>
       <div className="navbar-right">
-        <a href="#" className="navbar-link">Online Payment</a>
-        <a href="#" className="navbar-link-login active">Login</a>
+        <div className="SignUp">
+          <a href="#" className="navbar-link">Online Payment</a>
+          <a href="#" className="navbar-link-login active">Login</a>          
+        </div>
         <div className="language-section">
           <select>
             <option value="uz">O'zbekcha</option>
